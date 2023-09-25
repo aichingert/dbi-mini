184bc8a787b18666d861abe0751a9b58ea11aaa1
aichingert <aichinger.tobias13@gmail.com>
2023-09-25 12:35:06 +0200

    feat: script supports bind mounts now

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
+#!/bin/sh
+
+DIR="./db"
+
+if test ! -d $DIR; then
+	mkdir $DIR
+	chmod -R o+w $DIR
+fi
+
 podman run -d --name oradb \
 -p 1521:1521 \
 -e ORACLE_PWD=lol \
 -e ORACLE_CHARACTERSET=AL32UTF8 \
+-v $DIR:/opt/oracle/oradata \
 container-registry.oracle.com/database/free:latest
+
