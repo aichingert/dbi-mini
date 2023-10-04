868069a17433909b6179e6e0f131a45545b9b93a
aichingert <aichinger.tobias13@gmail.com>
2023-10-04 12:49:09 +0200

    feat: pl/sql added laying off an employee

--- plsql/pl-api.sql
+++ plsql/pl-api.sql
@@ -60,3 +60,23 @@ Exception
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Exception Thrown');
 END;
+
+CREATE OR REPLACE PROCEDURE LayOffEmployee(BRANCH_ID IN NUMBER, EMP_ID IN NUMBER, EMP_NAME IN VARCHAR2) IS
+    SEARCHING_NAME VARCHAR2(50);
+BEGIN
+    SELECT NAME INTO SEARCHING_NAME FROM EMPLOYEE
+    WHERE EMPLOYEEID = EMP_ID;
+
+    IF NOT (SEARCHING_NAME = EMP_NAME) THEN
+        DBMS_OUTPUT.PUT_LINE('Employee name does not match the Id (Are you sure you are removing the right employee?)');
+        RETURN;
+    END IF;
+
+    DELETE FROM EMPLOYEE WHERE BRANCHID = BRANCH_ID AND EMP_ID = EMPLOYEEID;
+
+    IF SQL%ROWCOUNT = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('Removed Employee ' || EMP_NAME || ' from branch ' || TO_CHAR(BRANCH_ID));
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Did not find employee in branch');
+    END IF;
+END;
